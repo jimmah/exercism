f95b4ceb04a6fa105bb6180f0afff4086cf43192
James Simm <james.simm@gmail.com>
2017-02-13 21:10:44 +0000

    Run Length Encoding - Elixir

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -7,12 +7,24 @@ defmodule RunLengthEncoder do
   "1H1O1R1S1E" => "HORSE"
   """
   @spec encode(String.t) :: String.t
-  def encode(string) do
-
+  def encode("" <> string) do
+    String.codepoints(string)
+    |> Enum.chunk_by(&(&1))
+    |> Enum.map_join(&encode_chars/1)
   end
 
   @spec decode(String.t) :: String.t
   def decode(string) do
+    Regex.scan(~r/(\d+)(\w)/, string, capture: :all_but_first)
+    |> Enum.map_join(&expand_pair/1)
+  end
 
+  defp encode_chars(chars) do
+    [Enum.count(chars), List.first(chars)]
+    |> Enum.join
+  end
+
+  defp expand_pair([count, char]) do
+    String.duplicate(char, String.to_integer(count))
   end
 end
