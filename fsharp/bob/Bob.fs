77af98b72d12321d82bef83509b0a17911c7b4f4
James Simm <james.simm@gmail.com>
2017-03-15 17:52:57 +0000

    F# - Bob

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,14 @@
+module Bob
+
+open System
+
+let hey(input: string) =
+    let isSilence = String.IsNullOrWhiteSpace input
+    let isShouting = Seq.exists Char.IsLetter input && input = input.ToUpperInvariant()
+    let isQuestion = input.EndsWith "?"
+
+    match input with
+        | _ when isSilence -> "Fine. Be that way!"
+        | _ when isShouting -> "Whoa, chill out!"
+        | _ when isQuestion -> "Sure."
+        | _ -> "Whatever."
