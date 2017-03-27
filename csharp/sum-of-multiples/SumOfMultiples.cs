2588897e902c84fbeb3aaa28089ecca290dc026e
James Simm <james.simm@gmail.com>
2017-03-27 18:02:28 +0100

    Sum of Multiples - C#

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int To(IEnumerable<int> multiples, int max)
+    {
+        return Enumerable.Range(1, max - 1)
+            .Where(x => multiples.Any(y => x % y == 0))
+            .Sum();
+    }
+}
