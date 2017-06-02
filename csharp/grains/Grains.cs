f232939442fefabed86c90e05ef4ee2af599ac31
James Simm <james.simm@gmail.com>
2017-06-02 21:37:11 +0100

    Grains - C#. Drunk on train.

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        return n == 1
+            ? 1
+            : 2 * Square(n - 1);
+    }
+
+    public static ulong Total()
+    {
+        ulong result = 0;
+
+        for (var i = 1; i <= 64; i++) {
+            result += Square(i);
+        }
+
+        return result;
+    }
+}
