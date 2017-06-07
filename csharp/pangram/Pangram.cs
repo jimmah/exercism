fa9dd21883faca4d792a5a4b25c2c45bf10e9345
James Simm <james.simm@gmail.com>
2017-06-07 17:58:47 +0100

    Pangram - C#

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+        => "abcdefghijklmnopqrstuvwxyz".Count(c => input.IndexOf(c) == -1) == 0;
+}
