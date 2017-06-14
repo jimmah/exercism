3bda44bb676e7077566b5a0f9856e86bebeb8675
James Simm <james.simm@gmail.com>
2017-06-14 17:34:54 +0100

    Raindrops - C#

---
+++ csharp/raindrops/Raindrops.cs
@@ -0,0 +1,23 @@
+using System;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        var result = "";
+
+        if (number % 3 == 0)
+            result += "Pling";
+        
+        if (number % 5 == 0)
+            result += "Plang";
+        
+        if (number % 7 == 0)
+            result += "Plong";
+        
+        if (string.IsNullOrEmpty(result))
+            result = number.ToString();
+
+        return result;
+    }
+}
