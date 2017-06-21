5771c7c17a790db27b13bb02d41397cccbb2bbec
James Simm <james.simm@gmail.com>
2017-06-21 17:35:03 +0100

    Bank Account - C#

---
+++ csharp/bank-account/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class BankAccount
+{
+    private readonly object _lock = new object();
+
+    private bool _isOpen;
+
+    private float _balance;
+
+    public void Open()
+    {
+        lock (_lock) 
+        {
+            _isOpen = true;
+        }
+    }
+
+    public void Close()
+    {
+        lock (_lock)
+        {
+            _isOpen = false;
+        }
+    }
+
+    public float Balance
+    {
+        get
+        {
+            lock (_lock) 
+            {
+                if (!_isOpen)
+                    throw new InvalidOperationException();
+
+                return _balance;
+            }
+        }
+    }
+
+    public void UpdateBalance(float change)
+    {
+        lock (_lock)
+        {
+            if (!_isOpen)
+                    throw new InvalidOperationException();
+
+            _balance += change;
+        }
+    }
+}
