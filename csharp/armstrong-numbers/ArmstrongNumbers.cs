59c1b786690b14332ecbeecc4b560c714fbf2769
James Simm <james.simm@gmail.com>
2018-12-31 16:52:47 +0000

    C# - Armstrong Numbers

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        var numberAsString = number.ToString();
+        var digits = numberAsString.Length;
+
+        var sum = 0d;
+        for (var i = 0; i < digits; i++)
+        {
+            sum += Math.Pow(int.Parse(numberAsString[i].ToString()), digits);
+        }
+
+        return number == sum;
+    }
+}
