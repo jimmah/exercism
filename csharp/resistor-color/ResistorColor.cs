9ec2062e99dbb02037d4d8dc8b7ff686a5c326d9
James Simm <james.simm@gmail.com>
2019-06-21 17:34:34 +0100

    C# - Resister Color

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public static class ResistorColor
+{
+    private static readonly List<string> _colors = new List<string>
+    {
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    };
+
+    public static int ColorCode(string color) => _colors.IndexOf(color);
+
+    public static string[] Colors() => _colors.ToArray();
+}
