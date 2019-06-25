edb27e9bb53f94684875b023b8d19c1a40177d34
James Simm <james.simm@gmail.com>
2019-06-25 09:57:00 +0100

    C# - Robot Simulator

---
+++ csharp/robot-simulator/RobotSimulator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
+
+public class RobotSimulator
+{
+    public RobotSimulator(Direction direction, int x, int y)
+    {
+        Direction = direction;
+        X = x;
+        Y = y;
+    }
+
+    public Direction Direction { get; private set; }
+
+    public int X { get; private set; }
+
+    public int Y { get; private set; }
+
+    public void Move(string instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            switch (instruction)
+            {
+                case 'L':
+                    Left();
+                    break;
+                case 'R':
+                    Right();
+                    break;
+                case 'A':
+                    Advance();
+                    break;
+            }
+        }
+    }
+
+    private void Left()
+    {
+        Direction = Direction == Direction.North ? Direction.West : (Direction - 1);
+    }
+
+    private void Right() 
+    {
+        Direction = Direction == Direction.West ? Direction.North : (Direction + 1);
+    }
+
+    private void Advance()
+    {
+        switch (Direction) 
+        {
+            case Direction.North:
+                Y++;
+                break;
+            case Direction.East:
+                X++;
+                break;
+            case Direction.South:
+                Y--;
+                break;
+            case Direction.West:
+                X--;
+                break;
+        }
+    }
+}
