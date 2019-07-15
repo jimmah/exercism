cd8687815a75a880430d03da837c1a40d2976ca1
James Simm <james.simm@gmail.com>
2019-07-15 17:50:27 +0100

    C# - Secret Handshake

---
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+public static class SecretHandshake
+{
+    private static string[] _actions = 
+    {
+        "wink",
+        "double blink",
+        "close your eyes",
+        "jump"
+    };
+
+    public static string[] Commands(int commandValue)
+    {
+        var actions = _actions.Where((x, i) => (1 << i & commandValue) > 0);
+        return (1 << 4 & commandValue) > 0 ? actions.Reverse().ToArray() : actions.ToArray();   
+    }
+}
