600b0cbc7c59422e0eed035a7f87ff227f90ad9f
James Simm <james.simm@gmail.com>
2019-07-15 17:33:10 +0100

    C# - Isogram

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var letters = new HashSet<char>();
+
+        foreach (var c in word.ToLowerInvariant().Where(Char.IsLetter)) 
+        {
+            if (!letters.Add(c))
+                return false;
+        }
+
+        return true;
+    }
+}
