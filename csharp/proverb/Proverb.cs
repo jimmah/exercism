0eb35da652271770ec12c90d9948f218fa191da6
James Simm <james.simm@gmail.com>
2019-07-17 17:43:28 +0100

    C# - Proverb

---
+++ csharp/proverb/Proverb.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        string Line(int index) => index == subjects.Length
+            ? $"And all for the want of a {subjects[0]}."
+            : $"For want of a {subjects[index - 1]} the {subjects[index]} was lost.";
+
+        return Enumerable.Range(1, subjects.Length).Select(Line).ToArray();
+    }
+}
