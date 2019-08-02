f83f58adfa1447707d4c6cb21f9fe24233830e3a
James Simm <james.simm@gmail.com>
2019-08-02 17:36:20 +0100

    C# - Circular Buffer

---
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CircularBuffer<T>
+{
+    private readonly int _capacity;
+    private Queue<T> _queue;
+
+    public CircularBuffer(int capacity)
+    {
+        _capacity = capacity;
+        _queue = new Queue<T>(capacity);
+    }
+
+    public T Read() => _queue.Dequeue();
+
+    public void Write(T value)
+    {
+        if (_queue.Count == _capacity)
+            throw new InvalidOperationException();
+
+        _queue.Enqueue(value);
+    }
+
+    public void Overwrite(T value)
+    {
+        if (_queue.Count < _capacity)
+        {
+            Write(value);
+        }
+        else 
+        {
+            _queue.Dequeue();
+            _queue = new Queue<T>(_queue.Append(value));
+        }
+    }
+
+    public void Clear() => _queue.Clear();
+}
